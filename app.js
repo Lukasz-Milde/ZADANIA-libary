61a782ce707b31ca54e2bb1fa8f6ef2b305bb85b
Lukasz-Milde <lukasz@milde.live>
2023-03-04 23:22:01 +0100

    user and book

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 console.log("dupa");
+
+class User {
+	name: string;
+	surname: string;
+  uuid: Number;
+    constructor(
+    name: string; 
+    surname: string; 
+    uuid: Number;) {
+    this.name = name;
+    this.surname = surname;
+    this.uuid = uuid;
+  }
+}
+
+class Book {
+  title: string;
+  author: string;
+  uuid: number;
+  randomPic: NIE WIEM;
+  description: string;
+  constructor(
+  title: string;
+  author: string;
+  uuid: number;
+  randomPic: NIE WIEM;
+  description: string;) {
+    this.title = title;
+    this.author = author;
+    this.uuid = uuid;
+    this.description = description;
+  }
+
+
+}
